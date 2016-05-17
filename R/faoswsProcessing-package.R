82b198515e4f7d01f89f0a127a645d42b14ed366
mk <mkao006@gmail.com>
2016-05-17 21:30:50 +0200

    updated package info

---
+++ R/faoswsProcessing-package.R
@@ -0,0 +1,13 @@
+##' Provide standardised processing procedures for data manipulation
+##'
+##' This package provides a family of functions to standardise data manipulation.
+##'
+##' @docType package
+##' @name faoswsProcessing-package
+##' @aliases faoswsProcessing
+##' @author Michael C. J. Kao
+##'
+##' @import data.table
+##'
+
+NULL
